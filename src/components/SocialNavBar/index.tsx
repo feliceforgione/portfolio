888c9c4afcde6178602fac09a1c41e938b61e472
Felice Forgione <forgione67@gmail.com>
2025-03-21 00:40:45 -0400

    Extract social media links

--- src/components/SocialNavBar/index.tsx
+++ src/components/SocialNavBar/index.tsx
@@ -1,43 +1,20 @@
+import { socialMediaLinks } from '@/config/links';
+import Image from 'next/image';
+
 interface Props {
   link: string;
   alt_text: string;
   image_file: string;
 }
 
-const socialMedia = [
-  {
-    link: 'https://www.linkedin.com/in/feliceforgione',
-    alt_text: 'linkedin',
-    icon: '/icons/linkedin.svg',
-  },
-  {
-    link: 'https://www.github.com/feliceforgione',
-    alt_text: 'github',
-    icon: '/icons/github.svg',
-  },
-  {
-    link: 'https://bsky.app/profile/feliceforgione.com',
-    alt_text: 'github',
-    icon: '/icons/bluesky.svg',
-  },
-  {
-    link: 'https://twitter.com/forgione67',
-    alt_text: 'twitter',
-    icon: '/icons/twitter.svg',
-  },
-  {
-    link: '/#contact',
-    alt_text: 'email',
-    icon: '/icons/email.svg',
-  },
-];
-
 function SocialIcon({ alt_text, image_file, link }: Props) {
   return (
     <a href={link}>
-      <img
+      <Image
         src={image_file}
         alt={alt_text}
+        width={24}
+        height={24}
         className=" w-6 md:w-8 mb-2 md:my-2 transition-all hover:opacity-40 transform hover:scale-75"
       />
     </a>
@@ -48,7 +25,7 @@ export default function SocialNavBar() {
   return (
     <div className="fixed left-3 md:left-5 bottom-0 z-30 bg-transparent">
       <div className="flex flex-col  justify-center items-center ">
-        {socialMedia.map((item) => {
+        {socialMediaLinks.map((item) => {
           return (
             <SocialIcon
               link={item.link}
--- src/config/links.ts
+++ src/config/links.ts
@@ -17,4 +17,32 @@ const navLinks = [
   },
 ];
 
+export const socialMediaLinks = [
+  {
+    link: 'https://www.linkedin.com/in/feliceforgione',
+    alt_text: 'linkedin',
+    icon: '/icons/linkedin.svg',
+  },
+  {
+    link: 'https://www.github.com/feliceforgione',
+    alt_text: 'github',
+    icon: '/icons/github.svg',
+  },
+  {
+    link: 'https://bsky.app/profile/feliceforgione.com',
+    alt_text: 'github',
+    icon: '/icons/bluesky.svg',
+  },
+  {
+    link: 'https://twitter.com/forgione67',
+    alt_text: 'twitter',
+    icon: '/icons/twitter.svg',
+  },
+  {
+    link: '/#contact',
+    alt_text: 'email',
+    icon: '/icons/email.svg',
+  },
+];
+
 export default navLinks;
